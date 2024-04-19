63b56e9bca5db4e8995fb490af76e7d757cefb54
José Francisco Díaz Castillo <jodiazc@microsoft.com>
2024-04-19 16:33:23 -0600

    aquí el cuarto

---
+++ starter/04 TACOCAT/tacocat/Program.cs
@@ -0,0 +1,21 @@
+﻿//crea una funcion que permita saber si una palabra es igual de derecha a izquierda que de izquierda a derecha
+
+//escribe el código que pregunte la palabra al usuario y muestre si es un palíndromo o no.
+
+Console.WriteLine("Escribe una palabra:");
+string palabra = Console.ReadLine();
+
+Console.WriteLine( validaPalindromo(palabra)); 
+
+static bool validaPalindromo(string palabra)
+{
+    string reversed = new string(palabra.Reverse().ToArray());
+    return QuitaEspacios( palabra).Equals(QuitaEspacios( reversed),  StringComparison.OrdinalIgnoreCase  );
+}
+
+//escribe el código que quite espacios a una cadena de texto
+
+static string QuitaEspacios(string frase)
+{
+    return frase.Replace(" ", "");
+}
